6946dc18e54951ae9465dd4fb3ba589cfc077d04
Sumaniya Devendra <dsumniya@gmail.com>
2023-09-22 19:19:59 +0530

    add new style and create new screen

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useRef, useState} from 'react';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import Voice from '@react-native-community/voice';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
+import {apiCall} from '../api/openAI';
+import Features from '../components/features';
+import Tts from 'react-native-tts';
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-white">
+      {/* <StatusBar barStyle="dark-content" /> */}
+      <SafeAreaView className="flex-1 flex mx-5">
+        {/* bot icon */}
+        <View className="flex-row justify-center">
+          <Image
+            source={require('../../assets/images/bot.png')}
+            style={{height: hp(15), width: hp(15)}}
+          />
+        </View>
+      </SafeAreaView>
+    </View>
+  );
+}
